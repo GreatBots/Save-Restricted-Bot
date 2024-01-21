b8387b8aa8882809e3015deda88df37a2f5972f4
Snowball <108003920+Snowball-0@users.noreply.github.com>
2024-01-21 15:09:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,4 +6,6 @@ class Config(object):
     API_HASH  = os.environ.get("API_HASH", "") # ⚠️ Required
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "") # ⚠️ Required
     STRING_SESSION = os.environ.get("STRING_SESSION", "") # ⚠️ Required
+    DB_URL = os.environ.get("DB_URL", "") # ⚠️ Required
+    DB_NAME = os.environ.get("DB_NAME", "TgBotData")
     PORT = int(os.environ.get("PORT", "8080"))
